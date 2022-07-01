454a65b3509207952d83940eb7ab7ea8e8a557b1
Rubotnic <info@rubotnic.com>
2022-07-01 16:48:56 +0200

    Cleaning code! ;)

--- src/test/java/se/lexicon/bankAccountTest.java
+++ src/test/java/se/lexicon/bankAccountTest.java
@@ -1,24 +1,37 @@
 package se.lexicon;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class bankAccountTest {
-
+    bankAccount bamsesAccount = new bankAccount();
     @Test
     void getId() {
+        bamsesAccount.setId("018411342");
+        String getId = bamsesAccount.getId();
+        Assertions.assertEquals("018411342", "018411342");
     }
 
     @Test
     void getBalance() {
+        bamsesAccount.setBalance(201);
+        int getBalance = bamsesAccount.getBalance();
+        Assertions.assertEquals(201,201);
     }
 
     @Test
     void deposit() {
+        bamsesAccount.setBalance(201);
+        bamsesAccount.deposit(1);
+        Assertions.assertTrue(true);
     }
 
     @Test
     void withdraw() {
+        bamsesAccount.setBalance(202);
+        bamsesAccount.withdraw(200);
+        Assertions.assertTrue(true);
     }
 }
